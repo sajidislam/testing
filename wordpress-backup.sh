f095d4eeff7d337c48aec27f7ee002fc0a4f25f2
Sajid Islam <sajidislam@gmail.com>
2023-11-30 12:13:32 -0500

    Update wordpress-backup.sh
    Modified the script so that the backup folder and the backup file name also contains the site domain name. 
    
    When the sitename is www.domain.com then the filename will start with www_domain and when the sitename is xyz.domain.com then the filename will start with xyz_domain. The date and time will be appended at the end of the filename
    
    In this script, the SITE_URL variable will get the site URL (like www.domain.com or xyz.domain.com). The DOMAIN_NAME variable will then process this URL to create a filename-friendly version (www_domain or xyz_domain). This domain name, along with the current date and time, is used in both the database backup filename and the WordPress directory archive filename.

--- wordpress-backup.sh
+++ wordpress-backup.sh
@@ -10,12 +10,16 @@ if [ "$1" == "-c" ]; then
     BACKUP_DIR="./backups"
 fi
 
-# Create a new directory with the current date and time
-BACKUP_PATH="$BACKUP_DIR/backup_$(date +"%Y%m%d_%H%M%S")"
-mkdir -p "$BACKUP_PATH"
-
 # Change to the WordPress directory
 cd "$WP_PATH"
+
+# Use WP-CLI to get the site domain and process it for the filename
+SITE_URL=$(wp option get home | awk -F/ '{print $3}')
+DOMAIN_NAME=$(echo $SITE_URL | awk -F. '{OFS="_"; print $1, $2}')
+
+# Create a new directory with the current date, time, and domain name
+BACKUP_PATH="$BACKUP_DIR/${DOMAIN_NAME}_backup_$(date +"%Y%m%d_%H%M%S")"
+mkdir -p "$BACKUP_PATH"
 
 # Read database credentials from wp-config.php
 DB_NAME=$(cat wp-config.php | grep DB_NAME | cut -d "'" -f 4)
@@ -24,9 +28,9 @@ DB_PASSWORD=$(cat wp-config.php | grep DB_PASSWORD | cut -d "'" -f 4)
 DB_HOST=$(cat wp-config.php | grep DB_HOST | cut -d "'" -f 4)
 
 # Perform MySQL dump
-mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_PATH/db-backup.sql"
+mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_PATH/${DOMAIN_NAME}_db-backup_$(date +"%Y%m%d_%H%M%S").sql"
 
 # Create a tar.gz archive of the WordPress directory
-tar -czvf "$BACKUP_PATH/wordpress-backup.tar.gz" .
+tar -czvf "$BACKUP_PATH/${DOMAIN_NAME}_wordpress-backup_$(date +"%Y%m%d_%H%M%S").tar.gz" .
 
 echo "Backup completed successfully. Files located at $BACKUP_PATH"
