90cc98d909fcf66ea784168ce2750da32e12173c
Sajid Islam <sajidislam@gmail.com>
2023-11-30 13:06:25 -0500

    Update wordpress-backup.sh
    This revised script:
    
    Checks for the -c parameter to set the WordPress and backup directory paths accordingly.
    Handles the cron true parameter to enable cron mode.
    Incorporates the error handling and email notification logic.

--- wordpress-backup.sh
+++ wordpress-backup.sh
@@ -1,47 +1,67 @@
 #!/bin/bash
 
 # Initialize default WP_PATH
-WP_PATH="/default/path/to/wordpress" # Update this with your default path
-BACKUP_DIR="/default/path/to/backups" # Update this with your default backup path
+WP_PATH="/default/path/to/wordpress"
+BACKUP_DIR="/default/path/to/backups"
+CRON_MODE=false
+ERROR_MESSAGE=""
+
+# Check for '-c' parameter
+if [ "$1" == "-c" ]; then 
+    WP_PATH="."
+    BACKUP_DIR="./backups"
+elif [ "$1" == "cron" ] && [ "$2" == "true" ]; then
+    CRON_MODE=true
+fi
 
 # Check for essential commands
-for cmd in mysqldump tar wp; do
+for cmd in mysqldump tar wp mail; do
     if ! command -v "$cmd" &> /dev/null; then
-        echo "Error: $cmd is not installed." >&2
-        exit 1
+        ERROR_MESSAGE="Error: $cmd is not installed."
+        echo "$ERROR_MESSAGE" >&2
+        break
     fi
 done
 
-# Check for '-c' parameter
-if [ "$1" == "-c" ]; then
-    WP_PATH="."
-    BACKUP_DIR="./backups"
+# Exit if any essential command is missing
+if [ -n "$ERROR_MESSAGE" ]; then
+    [ "$CRON_MODE" = true ] && echo "$ERROR_MESSAGE" | mail -s "WordPress Backup Script Error" "$(wp option get admin_email)"
+    exit 1
 fi
 
 # Change to the WordPress directory
 if ! cd "$WP_PATH"; then
-    echo "Error: Failed to change directory to $WP_PATH." >&2
+    ERROR_MESSAGE="Error: Failed to change directory to $WP_PATH."
+    echo "$ERROR_MESSAGE" >&2
+fi
+
+# Exit if changing directory fails
+if [ -n "$ERROR_MESSAGE" ]; then
+    [ "$CRON_MODE" = true ] && echo "$ERROR_MESSAGE" | mail -s "WordPress Backup Script Error" "$(wp option get admin_email)"
     exit 1
 fi
 
 # Use WP-CLI to get the site domain and process it for the filename
 SITE_URL=$(wp option get home 2>/dev/null)
 if [ $? -ne 0 ]; then
-    echo "WP-CLI failed to retrieve the site URL. Do you want to continue without the site name in the backup file name? [y/n]"
-    read -r user_input
-    if [ "$user_input" != "y" ]; then
-        echo "Script aborted by the user."
-        exit 1
-    fi
-    DOMAIN_NAME="backup"
-else
-    DOMAIN_NAME=$(echo $SITE_URL | awk -F/ '{print $3}' | awk -F. '{OFS="_"; print $1, $2}')
+    ERROR_MESSAGE="WP-CLI failed to retrieve the site URL."
+    echo "$ERROR_MESSAGE" >&2
+    [ "$CRON_MODE" = true ] && echo "$ERROR_MESSAGE" | mail -s "WordPress Backup Script Error" "$(wp option get admin_email)"
+    exit 1
 fi
+
+DOMAIN_NAME=$(echo $SITE_URL | awk -F/ '{print $3}' | awk -F. '{OFS="_"; print $1, $2}')
 
 # Create a new directory with the current date, time, and domain name
 BACKUP_PATH="$BACKUP_DIR/${DOMAIN_NAME}_$(date +"%Y%m%d_%H%M%S")"
 if ! mkdir -p "$BACKUP_PATH"; then
-    echo "Error: Failed to create backup directory $BACKUP_PATH." >&2
+    ERROR_MESSAGE="Error: Failed to create backup directory $BACKUP_PATH."
+    echo "$ERROR_MESSAGE" >&2
+fi
+
+# Exit if creating backup directory fails
+if [ -n "$ERROR_MESSAGE" ]; then
+    [ "$CRON_MODE" = true ] && echo "$ERROR_MESSAGE" | mail -s "WordPress Backup Script Error" "$(wp option get admin_email)"
     exit 1
 fi
 
@@ -53,31 +73,29 @@ DB_HOST=$(cat wp-config.php | grep DB_HOST | cut -d "'" -f 4)
 
 # Perform MySQL dump
 SQL_DUMP_FILE="$BACKUP_PATH/${DOMAIN_NAME}_db_$(date +"%Y%m%d_%H%M%S").sql"
-mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$SQL_DUMP_FILE"
-
-# Check mysqldump exit status
-if [ $? -ne 0 ]; then
-    echo "Error: MySQL dump failed." >&2
-    exit 1
-fi
-
-# Verify that the SQL dump file is not empty
-if [ ! -s "$SQL_DUMP_FILE" ]; then
-    echo "Error: The SQL dump file is empty." >&2
-    exit 1
-fi
-
-# Optionally, check for common MySQL error strings in the dump file
-if grep -q "MySQL: " "$SQL_DUMP_FILE"; then
-    echo "Warning: Potential errors found in the SQL dump file." >&2
-    # You may choose to exit or alert the user and continue
-    exit 1
+if ! mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$SQL_DUMP_FILE"; then
+    ERROR_MESSAGE="Error: MySQL dump failed."
+    echo "$ERROR_MESSAGE" >&2
 fi
 
 # Create a tar.gz archive of the WordPress directory
 if ! tar -czvf "$BACKUP_PATH/${DOMAIN_NAME}_wordpress_$(date +"%Y%m%d_%H%M%S").tar.gz" .; then
-    echo "Error: Failed to create tar.gz archive." >&2
-    exit 1
+    ERROR_MESSAGE="Error: Failed to create tar.gz archive."
+    echo "$ERROR_MESSAGE" >&2
 fi
 
-echo "Backup completed successfully. Files located at $BACKUP_PATH"
+# Final message and email notification
+if [ -n "$ERROR_MESSAGE" ]; then
+    FINAL_MESSAGE="The backup script executed. Here is the error it encountered: $ERROR_MESSAGE"
+else
+    FINAL_MESSAGE="The backup script executed and completed without any errors."
+fi
+
+echo "$FINAL_MESSAGE"
+
+if [ "$CRON_MODE" = true ]; then
+    echo "$FINAL_MESSAGE" | mail -s "WordPress Backup Script Execution" "$(wp option get admin_email)"
+fi
+
+# Exit with error status if there was an error
+[ -n "$ERROR_MESSAGE" ] && exit 1
